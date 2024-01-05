4afafd6244d5c8967892d83570213635ede2fbcf
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-05 11:42:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  const searchDiv = document.querySelector('.search');
+  const searchInput = document.querySelector('.input');
+  const searchButton = document.querySelector('.btn');
+
+  searchButton.addEventListener('click', function() {
+    searchDiv.classList.add('active');
+    searchInput.focus();
+  });
+});
